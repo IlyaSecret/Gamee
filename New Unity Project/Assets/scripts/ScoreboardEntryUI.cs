7835b04aaca4cc4c2b9df0e0d0856ebbc9a88d9f
IlyaSecret <ilyakosyakov1@yandex.ru>
2021-05-16 17:37:12 +0500

    scoreboard

--- New Unity Project/Assets/scripts/ScoreboardEntryUI.cs
+++ New Unity Project/Assets/scripts/ScoreboardEntryUI.cs
@@ -3,30 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+namespace Game.Scoreboards
+{
     public class ScoreboardEntryUI : MonoBehaviour
     {
         [SerializeField] private Text entryNameText = null;
         [SerializeField] private Text entryScoreText = null;
         [SerializeField] private Text entryPlaceText = null;
 
-        public void Initialise(string name, string place, string score)
+        public void Initialise(ScoreboardEntryData scoreboardEntryData)
         {
-            entryNameText.text = name;
-            entryPlaceText.text = place;
-            entryScoreText.text = score;
+            entryNameText.text = scoreboardEntryData.entryName;
+            entryPlaceText.text = scoreboardEntryData.entryPlace.ToString();
+            entryScoreText.text = scoreboardEntryData.entryScore.ToString();
         }
-    }
-
-
-public class ScoreData
-{
-    public readonly string name;
-    public readonly int score;
-
-    public ScoreData(string name, int score)
-    {
-        this.name = name;
-        this.score = score;
     }
 }
 
--- New Unity Project/Assets/scripts/Timer.cs
+++ New Unity Project/Assets/scripts/Timer.cs
@@ -5,20 +5,16 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public static float timeStart;
+    public float timeStart;
     public Text textTimer;
-    public Text score;
-    public Text finalScore;
     void Start()
     {
-        textTimer.text = timeStart.ToString();
+        textTimer.text = timeStart.ToString("F0");
     }
 
     void Update()
     {
         timeStart += Time.deltaTime;
-        textTimer.text = Mathf.Round(timeStart).ToString();
-        score.text = textTimer.text;
-        finalScore.text = textTimer.text;
+        textTimer.text = timeStart.ToString("F0");
     }
 }
